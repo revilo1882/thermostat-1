b5129dc62410b5b2126de4cd00612a7cd7d378f7
Revilo1882 <oliverrharris@gmail.com>
2018-05-17 15:20:50 +0100

    adds functionality to all buttons

--- src/thermostat-interface.js
+++ src/thermostat-interface.js
@@ -10,4 +10,19 @@ $(document).ready(function() {
     thermostat.down();
     console.log(thermostat.temperature);
   });
+
+  $('#temperature-reset').click(function() {
+    thermostat.reset();
+    console.log(thermostat.temperature);
+  });
+
+  $('#power-saving').click(function() {
+    thermostat.togglePowerSaving();
+    console.log(thermostat.powerSavingIsOn);
+    console.log(thermostat.temperature);
+  });
+
+  $('#power-usage').click(function() {
+    console.log(thermostat.energyUsage());
+  });
 });
